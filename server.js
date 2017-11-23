7644d423a88951aa6b04bcba29cf907b964bf575
Kerwin Jorbina <kerwin.jorbina@gmail.com>
2017-11-23 03:05:20 -0500

    fix refreshing error

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // server.js
 const express = require('express');
+const path = require('path')
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+})
+
 app.listen(process.env.PORT || 8080);
